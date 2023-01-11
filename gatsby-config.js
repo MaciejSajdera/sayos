0c6bb4ba8118f9ce62ddab26e45198b7f153400a
Maciek <msajdera@gmail.com>
2023-01-11 11:14:04 +0100

    gatsby-plugin-netlify installed.

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     `gatsby-plugin-image`,
     // `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify`,
     {
       resolve: "gatsby-plugin-sass",
     },
